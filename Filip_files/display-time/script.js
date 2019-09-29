3d6c163fc94523727c35b0b86b610f5be88326dd
fzajic <filip.zajic007@gmail.com>
2019-09-29 09:29:26 +0200

    Commiting wortking version of my experimental web page that shows time.

--- Filip_files/display-time/script.js
+++ Filip_files/display-time/script.js
@@ -1,9 +1,5 @@
 function main() {
-  // line belows gives an error: Uncaught ReferenceError: $ is not defined
-  // how schould the code be changed?
-  // the line is suppose to hide the time
-  // the time should be showed after a click on the "timme-button"
-  //$(".time").hide();
+  $(".time").hide();
   var d = new Date();
   var h = d.getHours();
   // this ensures the hh:mm format
@@ -13,13 +9,15 @@ function main() {
   var m = d.getMinutes();
   // this ensures the hh:mm format
   if (m.toString().length == 1) {
-    m = "0" + m
+    m = "0" + m;
   }
-
-  d = h + " : " + m
+  d = h + " : " + m;
   document.getElementById("hour").innerHTML = d;
-  // does not work as well
-  $(".time").fadeIn(1000);
+
+  $(".time-button").on("click", function() {
+    $(this).next().slideToggle(400);
+    $(this).text("Hide time by clicking the button");
+  });
 }
 
-main()
+main();
