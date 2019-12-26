d2b77b06b8da5e1d2dda2e87c9b7dfb1f964d513
fzajic <filip.zajic007@gmail.com>
2019-12-26 21:01:06 +0100

    function that ads history log to page, functions for date calculations, basic css, html classes and IDs

--- user-page/user_homepage/script.js
+++ user-page/user_homepage/script.js
@@ -1,21 +1,11 @@
+//fake data used throughout the script for testing
+//those will be later replaced by real data from database via API
 var data = {
     "date": "2019-12-13",
     "time_in": "15-01-00",
     "time_out": "16-20-00"
 };
 
-// var time = data.time_in;
-// var date = data.date;
-
-// console.log(date);
-// console.log(time);
-
-
-
-
-
-
-console.log(getDayName(data));
 
 //get name of day based on date
 function getDayName(data) {
@@ -62,9 +52,9 @@ function getMonthName(data) {
     month = month - 1; //substract one because first index of list is 0 not 1
     month = monthList[month]; //find name of month that equals to the number
     return month;
-}
+};
 
-//get date of day (output = 15, only the number, not 'th of 'rd)
+//get date of day (exapmle -> output = 15, only the number, not 'th or 'rd)
 function getDayNumber(data) {
     var day = "";
     for (let i = 8; i < 10; i++) {
@@ -78,8 +68,8 @@ function getHourInNumber(data) {
     var hour = "";
     for (let i = 0; i < 2; i++) {
         hour = hour + data.time_in[i];
-    }
-    return hour
+    };
+    return hour;
 }
 
 //get hour of departure from data
@@ -87,8 +77,8 @@ function getHourOutNumber(data) {
     var hour = "";
     for (let i = 0; i < 2; i++) {
         hour = hour + data.time_out[i];
-    }
-    return hour
+    };
+    return hour;
 }
 
 //get minute of arrival from data
@@ -96,26 +86,72 @@ function getMinuteInNumber(data) {
     var minute = "";
     for (let i = 3; i < 5; i++) {
         minute = minute + data.time_in[i];
-    }
-    return minute
-}
+    };
+    return minute;
+};
 
 // get minute of departure from data
 function getMinuteOutNumber(data) {
     var minute = "";
     for (let i = 3; i < 5; i++) {
         minute = minute + data.time_out[i];
-    }
-    return minute
-}
-
-
-// function that ads content to page
-function addLog(jsonInput) {
-
-    var x = document.createElement("P");
-    var t = document.createTextNode("Adding placeholder");
-    x.appendChild(t);
-
-    document.getElementById("history").appendChild(x);
+    };
+    return minute;
 };
+
+function durationOfStay(data) {
+    //                                                  minutes *1 to make sure its integer and not string
+    var minutesStayed = ((getHourOutNumber(data) * 60) + getMinuteOutNumber(data) * 1) - ((getHourInNumber(data) * 60) + getMinuteInNumber(data) * 1);
+    var hoursStayed = Math.floor(minutesStayed / 60);
+    minutesStayed = minutesStayed - hoursStayed * 60;
+    var duration = hoursStayed + ":" + minutesStayed;
+    return duration;
+
+
+};
+
+//add History Log
+function addHistoryLog(data, divId) {
+    var newDiv = document.createElement("div");
+    newDiv.id = divId;
+    newDiv.className = "logBoxWrap";
+    document.getElementById("history").appendChild(newDiv);
+
+    // get individual informations from input data and combine them with wording
+    var sessionDate = getDayName(data) + " " + getDayNumber(data) + ". " + getMonthNumber(data) + ". " + getYearNumber(data);
+    var arrival = "Arrived at " + getHourInNumber(data) + ":" + getMinuteInNumber(data);
+    var departure = "Departured at " + getHourOutNumber(data) + ":" + getMinuteOutNumber(data);
+    var duration = "Stayed for " + durationOfStay(data);
+
+    //create list of future rows for easier access
+    var historyLogList = [sessionDate, arrival, departure, duration];
+
+    for (let index = 0; index < historyLogList.length; index++) {
+        let text = historyLogList[index];
+
+        let node = document.createElement("P");
+
+        //to add only class to first <p> - to make it bolder
+        if (index === 0) {
+            node.className = "sessionName";
+        };
+
+        var textNode = document.createTextNode(text);
+        node.appendChild(textNode);
+        document.getElementById(divId).appendChild(node);
+    };
+};
+
+
+
+addHistoryLog(data, "session1");
+
+addHistoryLog(data, "session2");
+
+durationOfStay(data);
+
+
+// for (let i = 0; i < 5; i++) {
+//     addHistoryLog(data, "session " + i);
+
+// }
